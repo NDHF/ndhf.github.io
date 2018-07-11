917da71cf14777770e96effa0e6bcd1b3fcc588d
Nicholas Bernhard <nicholas@NDHFilms.com>
2018-07-10 18:55:03 -0600

    Adding functionality to speed trap test

--- speedtraptest/logic.js
+++ speedtraptest/logic.js
@@ -1,17 +1,17 @@
 $(document).on("click", "#submitButton", function () {
-            console.log("test");
+    console.log("test");
 
-            //Key for the Google API:
-            // var APIKey = "AIzaSyB-cP9CrYbHOtYNDY2_aNHIzDjR3DvHjwE";
+    //Key for the Google API:
+    // var APIKey = "AIzaSyB-cP9CrYbHOtYNDY2_aNHIzDjR3DvHjwE";
 
 
-            //Declaring the Giphy API's query URL as a variable, incorporating our API key and search term
-            //The "&limit5" will produce 5 results per click
-            // var queryURL = "https://roads.googleapis.com/v1/snapToRoads?path=-35.27801,149.12958|-35.28032,149.12907|-35.28099,149.12929|-35.28144,149.12984|-35.28194,149.13003|-35.28282,149.12956|-35.28302,149.12881|-35.28473,149.12836&interpolate=true&key=" + APIKey;
-            var parameters = "placeId=ChIJX12duJAwGQ0Ra0d4Oi4jOGE&placeId=ChIJLQcticc0GQ0RoiNZJVa5GxU";
-            var queryURL = "https://roads.googleapis.com/v1/speedLimits?" + parameters + "&key=" + APIKey;
+    //Declaring the Giphy API's query URL as a variable, incorporating our API key and search term
+    //The "&limit5" will produce 5 results per click
+    // var queryURL = "https://roads.googleapis.com/v1/snapToRoads?path=-35.27801,149.12958|-35.28032,149.12907|-35.28099,149.12929|-35.28144,149.12984|-35.28194,149.13003|-35.28282,149.12956|-35.28302,149.12881|-35.28473,149.12836&interpolate=true&key=" + APIKey;
+    var parameters = "placeId=ChIJX12duJAwGQ0Ra0d4Oi4jOGE&placeId=ChIJLQcticc0GQ0RoiNZJVa5GxU";
+    var queryURL = "https://roads.googleapis.com/v1/speedLimits?" + parameters + "&key=" + APIKey;
 
-            console.log(queryURL);
+    console.log(queryURL);
 
 });
 
@@ -19,14 +19,18 @@ var x;
 
 var y;
 
-var coordinateArray = [{name: "denverUnionStation", xCoordinate: 39.75, yCoordinate: -105.00}];
+var coordinateArray = [{
+    name: "denverUnionStation",
+    xCoordinate: 39.75,
+    yCoordinate: -105.00
+}];
 
 function getLocation() {
 
     console.log("Location obtained");
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
-    } else { 
+    } else {
         $("#location").html("Geolocation is not supported by this browser.");
     }
 }
@@ -35,8 +39,8 @@ function showPosition(position) {
     x = position.coords.longitude;
     y = position.coords.latitude;
     console.log(typeof x);
-    $("#location").html("Latitude: " + "<span id='xPos'>" + x.toFixed(2) + "</span>" + 
-    "<br>Longitude: " + "<span id='yPos'>" + y.toFixed(2) + "</span>"); 
+    $("#location").html("Latitude: " + "<span id='xPos'>" + x + "</span>" +
+        "<br>Longitude: " + "<span id='yPos'>" + y + "</span>");
     console.log("test");
 };
 
@@ -55,8 +59,18 @@ function newValues() {
     newY = $("#yPos")
 
     direction();
-    
+    proximityCheck();
+
 };
+
+var alertSound = new Audio('assets/audio/richEvansLaugh01.mp3');
+
+$("#playAlert").on("click", function () {
+
+    alertSound.play();
+
+});
+
 
 setInterval(newValues, 60000);
 
@@ -87,7 +101,7 @@ function direction() {
     } else if ((newX < oldX) && (newY > oldY)) {
         direction = "northwest";
         $("#direction").html(direction);
-    } 
+    }
 }
 
 //This code looks for changes in the second decimal point, which, on a map, is accurate to 1.1 KM, or .68 miles.
@@ -95,37 +109,26 @@ function direction() {
 //I have broken this code into blocks for now, to work it would need to be called as part of a for-loop that looped
 //through an array of coordinates. The for-loop functon can be called using a setInterval.
 
-function proximityAlert() {
-    if ((direction === "north") && ((newY < coordinateArray[i].yCoordinate) && (newY  > (coordinateArray[i].yCoordinate - 0.01))) && (newX === coordinateArray[i].xCoordinate)) {
-        return proximitySound; 
-    } 
+function proximityCheck() {
+    for (i = 0; i <= coordinateArray.length; i++) {
 
-    if ((direction === "south") && ((Ycurrent > Yahead) && (Ycurrent  < (Yahead + 0.01))) && (Xcurrent === Xahead)) {
-        return proximitySound; 
-    } 
+        if ((direction === "north") && ((newY < coordinateArray[i].yCoordinate) && (newY > (coordinateArray[i].yCoordinate - 0.01))) && (newX === coordinateArray[i].xCoordinate)) {
+            alertSound.play();
+        } else if ((direction === "south") && ((newY > coordinateArray[i].yCoordinate) && (newY < (coordinateArray[i].yCoordinate + 0.01))) && (newX === coordinateArray[i].xCoordinate)) {
+            alertSound.play();
+        } else if ((direction === "west") && ((newX > coordinateArray[i].xCoordinate) && (newX < (coordinateArray[i].xCoordinate + 0.01))) && (newX === coordinateArray[i].xCoordinate)) {
+            alertSound.play();
+        } else if ((direction === "east") && ((newX < coordinateArray[i].xCoordinate) && (newX > (coordinateArray[i].xCoordinate - 0.01))) && (newY === coordinateArray[i].yCoordinate)) {
+            alertSound.play();
+        } else if ((direction === "northeast") && ((newY < coordinateArray[i].yCoordinate) && (newY > (coordinateArray[i].yCoordinate - 0.01))) && ((newX < coordinateArray[i].xCoordinate) && (newX > (coordinateArray[i].xCoordinate - 0.01)))) {
+            alertSound.play();
+        } else if ((direction === "southeast") && ((newY > coordinateArray[i].yCoordinate) && (newY < (coordinateArray[i].yCoordinate + 0.01))) && ((newX < coordinateArray[i].xCoordinate) && (newX > (coordinateArray[i].xCoordinate - 0.01)))) {
+            alertSound.play();
+        } else if ((direction === "southwest") && ((newY > coordinateArray[i].yCoordinate) && (newY < (coordinateArray[i].yCoordinate + 0.01))) && ((newX > coordinateArray[i].xCoordinate) && (newX < (coordinateArray[i].xCoordinate + 0.01)))) {
+            alertSound.play();
+        } else if ((direction === "northwest") && ((newY < coordinateArray[i].yCoordinate) && (newY > (coordinateArray[i].yCoordinate - 0.01))) && ((newX > coordinateArray[i].xCoordinate) && (newX < (coordinateArray[i].xCoordinate + 0.01)))) {
+            alertSound.play();
+        }
 
-    if ((direction === "west") && ((Xcurrent > Xahead) && (Xcurrent  < (Xahead + 0.01))) && (Ycurrent === Yahead)) {
-        return proximitySound; 
-    }
-    
-    if ((direction === "east") && ((Xcurrent < Xahead) && (Xcurrent > (Xahead - 0.01))) && (Ycurrent === Yahead)) {
-        return proximitySound; 
-    }
-
-    if ((direction === "northeast") && ((Ycurrent < Yahead) && (Ycurrent  > (Yahead - 0.01))) && ((Xcurrent < Xahead) && (Xcurrent > (Xahead - 0.01)))) {
-        return proximitySound; 
-    }  
-
-    if ((direction === "southeast") && ((Ycurrent > Yahead) && (Ycurrent  < (Yahead + 0.01))) && ((Xcurrent < Xahead) && (Xcurrent > (Xahead - 0.01)))) {
-        return proximitySound; 
-    }  
-
-    if ((direction === "southwest") && ((Ycurrent > Yahead) && (Ycurrent  < (Yahead + 0.01))) && ((Xcurrent > Xahead) && (Xcurrent  < (Xahead + 0.01)))) {
-        return proximitySound; 
-    }
-
-    if ((direction === "northwest") && ((Ycurrent < Yahead) && (Ycurrent  > (Yahead - 0.01))) && ((Xcurrent > Xahead) && (Xcurrent  < (Xahead + 0.01)))) {
-        return proximitySound; 
     }
 }
-
