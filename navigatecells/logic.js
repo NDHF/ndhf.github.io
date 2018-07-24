b54ed7e20c3601e897aff5455c9ffa7a7b343fdd
Nicholas Bernhard <nicholas@NDHFilms.com>
2018-07-23 18:46:11 -0600

    Updates to navigate-cells program

--- navigatecells/logic.js
+++ navigatecells/logic.js
@@ -1,20 +1,24 @@
-// A simple function for navigating a multidimensional array
+// A simple program for navigating a multidimensional array
 
 //Even though I am navigating in two-dimensional space, my array has three dimensions:
     //One for going up and down
     //One for going side to side
     //One for rotating in place
 
-//This array will attempt to approximate the museum I work at:
 
 // Within the sub-sub-arrays:
     //           0 = north
     // west = 3     1 = east
     //           2 = south
 
-// For example, each index in the array can be an html img tag of a perspective in a room, allowing the user to click through an area in Myst-fashion
+// For example, each index in the array can be an HTML img tag of a perspective in a room, 
+// allowing the user to click through an area, like in the game Myst
 
-console.log("Hello!");
+// GLOBAL VARIABLES AND FUNCTIONS START
+
+//Perhaps some of these need not be global?
+
+//This array will attempt to approximate the museum where I work:
 
 var museumArray = [
 
@@ -28,52 +32,120 @@ var museumArray = [
 
 ];
 
-
-
-
-// //This function will draw an HTML table of the same dimensions as the array. The CSS file defines the styling of each cell.
-// function mapGenerator() {
-//     $("#mapContainer").append("<table></table>");
-//     $("#mapContainer table").addClass("map");
-    
-//     for (i = 0; i <= museumArray.length - 1; i++) {
-//         $(".map").append("<tr></tr>");
-//         $(".map tr").addClass("row");
-        
-//     }
-
-//     for (j = 0; j <= museumArray[0].length - 1; j++) {
-//         $(".row").append("<td></td>");
-//     }
-
-// };
-
-// mapGenerator();
-
-// //This function can be the starting point for a way to go through the drawn array, and apply it to the HTML table
-// //The objective is to only draw the cells that the user can navigate, leaving all the undefined arrays blank
-// function checkArray() {
-//     for (i = 0; i <= museumArray.length - 1; i++) {
-//         for (j = 0; j <= museumArray[i].length - 1; j++) {
-//             if (museumArray[i][j][0] === undefined) {
-//                 console.log("undefined");
-//             } else {
-//                 console.log("defined");
-//             }
-//         }
-//     }
-// };
-
 var x = 5;
 var y = 2;
 var z = 0;
 
+var canMoveForward;
+var canMoveBackward;
+
+infoPackage();
+
+// GLOBAL VARIABLES AND FUNCTIONS END
+
 function showCoordinates() {
     console.log(x + "," + y + "," + z);
-}
+};
 
-showCoordinates();
-getLocation();
+// Check if an adjacent array is out-of-bounds
+
+function checkMovements() {
+
+    if (z === 0) {
+        if (museumArray[(x - 1)][y][z] === undefined) {
+            buttonStatus("#forwardButton", "off");
+            canMoveForward = false;
+        } else {
+            buttonStatus("#forwardButton", "on");
+            canMoveForward = true;
+        }
+
+        if (museumArray[(x + 1)][y][z] === undefined) {
+            buttonStatus("#backwardButton", "off");
+            canMoveBackward = false;
+        } else {
+            buttonStatus("#backwardButton", "on");
+            canMoveBackward = true; 
+        }
+    }
+
+    if (z === 1) {
+        if (museumArray[x][(y + 1)][z] === undefined) {
+            buttonStatus("#forwardButton", "off");
+            canMoveForward = false;
+        } else {
+            buttonStatus("#forwardButton", "on");
+            canMoveForward = true;
+        }
+
+        if (museumArray[x][(y - 1)][z] === undefined) {
+            buttonStatus("#backwardButton", "off");
+            canMoveBackward = false;
+        } else {
+            buttonStatus("#backwardButton", "on");
+            canMoveBackward = true;
+        }
+    }
+
+    if (z === 2) {
+        if (museumArray[(x + 1)][y][z] === undefined) {
+            buttonStatus("#forwardButton", "off");
+            canMoveForward = false;
+        } else {
+            buttonStatus("#forwardButton", "on");
+            canMoveForward = true;
+        }
+
+        if (museumArray[(x - 1)][y][z] === undefined) {
+            buttonStatus("#backwardButton", "off");
+            canMoveBackward = false;
+        } else {
+            buttonStatus("#backwardButton", "on");
+            canMoveBackward = true;
+        }
+    }
+
+    if (z === 3) {
+        if (museumArray[x][(y - 1)][z] === undefined) {
+            buttonStatus("#forwardButton", "off");
+            canMoveForward = false;
+        } else {
+            buttonStatus("#forwardButton", "on");
+            canMoveForward = true;
+        }
+
+        if (museumArray[x][(y + 1)][z] === undefined) {
+            buttonStatus("#backwardButton", "off");
+            canMoveBackward = false;
+        } else {
+            buttonStatus("#backwardButton", "on");
+            canMoveBackward = true;
+        }
+    }
+};
+
+// Provide visual cues to user for whether they can or can't move in a certain direction
+// Here, the visual cue is a grayed-out button
+
+function buttonStatus(button, onOrOff) {
+
+    if (onOrOff === "off") {
+    $(button).css("background-color", "gray");
+    } else if (onOrOff === "on") {
+    $(button).css("background-color", "blue");
+    }
+};
+
+// These functions are called each time a player calls the navigate() function,
+// so they are bundled together. 
+
+function infoPackage() {
+    showCoordinates();
+    checkMovements();
+    getLocation();
+};
+
+// jQuery selectors for navigation buttons
 
 $("#forwardButton").on("click", function() {
     navigate("forward");
@@ -84,207 +156,110 @@ $("#backwardButton").on("click", function() {
 });
 
 $("#leftButton").on("click", function() {
-    navigate("left");
-});
-
-$("#rightButton").on("click", function() {
-    navigate("right");
-});
-
-$("#rotateLeftButton").on("click", function() {
     navigate("ccw");
 });
 
-$("#rotateRightButton").on("click", function() {
+$("#rightButton").on("click", function() {
     navigate("cw");
 });
 
 function navigate(direction) {
 
-if (z === 0) {
-    if (direction === "forward") {
-        if (museumArray[(x - 1)][y][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            x--;
-            showCoordinates();
-            getLocation();
-        }
-    } else if (direction === "backward") {
-        if (museumArray[(x + 1)][y][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            x++;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "left") {
-        if (museumArray[x][(y - 1)][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            y--;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "right") {
-        if (museumArray[x][(y + 1)][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation(); 
-        } else {
-            y++;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    }
-
-} else if (z === 1) {
-    if (direction === "forward") {
-        if (museumArray[x][(y + 1)][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation(); 
-        } else {
-            y++;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "backward") {
-        if (museumArray[x][(y - 1)][z] === undefined) {
-            console.log("Not permitted");
-            getLocation();
-        } else {
-            y--;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "left") {
-        if (museumArray[(x - 1)][y][z] === undefined) {
-            console.log("Not permitted");
-            getLocation();
-        } else {
-            x--;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "right") {
-        if (museumArray[(x + 1)][y][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            x++;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    }
-
-} else if (z === 2) {
-    if (direction === "forward") {
-        if (museumArray[(x + 1)][y][z] === undefined) {
-            console.log("Not permitted");  
-            getLocation();
-        } else {
-            x++;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "backward") {
-        if (museumArray[(x - 1)][y][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            x--;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "left") {
-        if (museumArray[x][(y + 1)][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            y++;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "right") {
-        if (museumArray[x][(y - 1)][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            y--;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    }
-
-} else if (z === 3) {
-    if (direction === "forward") {
-        if (museumArray[x][(y - 1)][z] === undefined) {
-            console.log("Not permitted");
-            getLocation();
-        } else {
-            y--;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "backward") {
-        if (museumArray[x][(y + 1)][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            y++;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "left") {
-        if (museumArray[(x + 1)][y][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            x++;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    } else if (direction === "right") {
-        if (museumArray[x - 1][y][z] === undefined) {
-            console.log("Not permitted"); 
-            getLocation();
-        } else {
-            x--;
-            console.log(x + "," + y + "," + z);
-            getLocation();
-        }
-    }
-
-}
-
-if (direction === "cw") {
-    if (z === 3) {
-        z = 0;
-        console.log(x + "," + y + "," + z);
-        getLocation();
-    } else {
-        z++;
-     console.log(x + "," + y + "," + z);
-     getLocation();
-    }
-}
-
-if (direction === "ccw") {
     if (z === 0) {
-        z = 3;
-        console.log(x + "," + y + "," + z);
-        getLocation();
-    } else {
-        z--;
-        console.log(x + "," + y + "," + z);
-        getLocation();
+        if (direction === "forward") {
+            if (canMoveForward === false) {
+                console.log("Not permitted"); 
+            infoPackage();
+            } else if (canMoveForward === true) {
+                x--;
+                infoPackage();
+            }
+        } else if (direction === "backward") {
+            if (canMoveBackward === false) {
+                console.log("Not permitted"); 
+                infoPackage();
+            } else if (canMoveBackward === true) {
+                x++;
+                infoPackage();
+            }
+        } 
+    } else if (z === 1) {
+        if (direction === "forward") {
+            if (canMoveForward === false) {
+                console.log("Not permitted"); 
+                infoPackage();
+            } else if (canMoveForward === true) {
+                y++;
+                infoPackage();
+            }
+        } else if (direction === "backward") {
+            if (canMoveBackward === false) {
+                console.log("Not permitted");
+                infoPackage();
+            } else if (canMoveBackward === true) {
+                y--;
+                infoPackage();
+            }
+        }
+    } else if (z === 2) {
+        if (direction === "forward") {
+            if (canMoveForward === false) {
+                console.log("Not permitted");  
+                infoPackage();
+            } else if (canMoveForward === true) {
+                x++;
+                infoPackage();
+            }
+        } else if (direction === "backward") {
+            if (canMoveBackward === false) {
+                console.log("Not permitted"); 
+                infoPackage();
+            } else if (canMoveBackward === true) {
+                x--;
+                infoPackage();
+            }
+        } 
+    } else if (z === 3) {
+        if (direction === "forward") {
+            if (canMoveForward === false) {
+                console.log("Not permitted");
+                infoPackage();
+            } else if (canMoveForward === true) {
+                y--;
+                infoPackage();
+            }
+        } else if (direction === "backward") {
+            if (canMoveBackward === false) {
+                console.log("Not permitted"); 
+                infoPackage();
+            } else if (canMoveBackward === true) {
+                y++;
+                console.log(x + "," + y + "," + z);
+                infoPackage();
+            }
+        } 
     }
-
-}
-
-    }; 
+    
+    if (direction === "cw") {
+        if (z === 3) {
+            z = 0;
+            infoPackage();
+        } else {
+            z++;
+            infoPackage();
+        }
+    }
+    
+    if (direction === "ccw") {
+        if (z === 0) {
+            z = 3;
+            infoPackage();
+        } else {
+            z--;
+            infoPackage();
+        }  
+    }   
+}; 
 
 //This function will give the user a message based on their position.
 //The general if-statement takes two coordinates to show which cell the user is in.
@@ -303,7 +278,6 @@ function getLocation() {
         } else if (z === 3) {
             console.log("You are looking east on Simpson Street, toward the historic business district.");
         } 
-
     }
 
     if ((x === 4) && (y === 1)) {
@@ -358,8 +332,7 @@ function getLocation() {
         } else if (z === 3) {
             console.log("You are looking at our sliding display, with a collection of facts and photos from city history.");
             $("#picture").attr("src", "images/x3y1z3.JPG");
-        } 
-        
+        }    
     }
 
     if ((x === 3) && (y === 2)) {
@@ -377,9 +350,7 @@ function getLocation() {
         } else if (z === 3) {
             console.log("You are looking at our upright mine tool display, featuring hard hats, lanterns, and samples of coal. To the right of the case, you may enter the high school exhibit.");
             $("#picture").attr("src", "images/x3y2z3.JPG");
-
-        } 
-        
+        }    
     }
 
     if ((x === 2) && (y === 2)) {
@@ -417,5 +388,4 @@ function getLocation() {
             $("#picture").attr("src", "images/x1y2z3.JPG");
         } 
     }
-
 };
---
+++ navigatecells/oldLogic.js
@@ -0,0 +1,274 @@
+//An older version of the navigate() function. 
+//This version checks if adjacent arrays are undefined before attempting to navigate.
+//This logic is now used in the checkMovement() function.
+
+//checkMovement() also gives users visual cues on where they can and can't move, by graying-out impermissible directions.
+
+//As of 07/23/2018, checkMovement() assigns a boolean value to variables canMoveForward and canMoveBackward
+//The newer navigate() function checks the boolean values before attempting navigation
+
+//This function also includes logic for strafing, rather than rotating, left and right. 
+//I found most users didn't make use of it, so that code is not being used right now
+
+function navigate(direction) {
+
+if (z === 0) {
+    if (direction === "forward") {
+        if (museumArray[(x - 1)][y][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+            
+        } else {
+            x--;
+            showCoordinates();
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "backward") {
+        if (museumArray[(x + 1)][y][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            x++;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "left") {
+        if (museumArray[x][(y - 1)][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            y--;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "right") {
+        if (museumArray[x][(y + 1)][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation(); 
+        } else {
+            y++;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    }
+
+} else if (z === 1) {
+    if (direction === "forward") {
+        if (museumArray[x][(y + 1)][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation(); 
+        } else {
+            y++;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "backward") {
+        if (museumArray[x][(y - 1)][z] === undefined) {
+            console.log("Not permitted");
+            getLocation();
+        } else {
+            y--;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "left") {
+        if (museumArray[(x - 1)][y][z] === undefined) {
+            console.log("Not permitted");
+            getLocation();
+        } else {
+            x--;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+        }
+    } else if (direction === "right") {
+        if (museumArray[(x + 1)][y][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            x++;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    }
+
+} else if (z === 2) {
+    if (direction === "forward") {
+        if (museumArray[(x + 1)][y][z] === undefined) {
+            console.log("Not permitted");  
+            getLocation();
+        } else {
+            x++;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "backward") {
+        if (museumArray[(x - 1)][y][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            x--;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "left") {
+        if (museumArray[x][(y + 1)][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            y++;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "right") {
+        if (museumArray[x][(y - 1)][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            y--;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    }
+
+} else if (z === 3) {
+    if (direction === "forward") {
+        if (museumArray[x][(y - 1)][z] === undefined) {
+            console.log("Not permitted");
+            getLocation();
+        } else {
+            y--;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "backward") {
+        if (museumArray[x][(y + 1)][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            y++;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "left") {
+        if (museumArray[(x + 1)][y][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            x++;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    } else if (direction === "right") {
+        if (museumArray[x - 1][y][z] === undefined) {
+            console.log("Not permitted"); 
+            getLocation();
+        } else {
+            x--;
+            console.log(x + "," + y + "," + z);
+            getLocation();
+            checkMovements();
+        }
+    }
+
+}
+
+if (direction === "cw") {
+    if (z === 3) {
+        z = 0;
+        console.log(x + "," + y + "," + z);
+        getLocation();
+        checkMovements();
+    } else {
+        z++;
+     console.log(x + "," + y + "," + z);
+     getLocation();
+     checkMovements();
+    }
+}
+
+if (direction === "ccw") {
+    if (z === 0) {
+        z = 3;
+        console.log(x + "," + y + "," + z);
+        getLocation();
+        checkMovements();
+    } else {
+        z--;
+        console.log(x + "," + y + "," + z);
+        getLocation();
+        checkMovements();
+    }
+
+}
+
+};
+
+// jQuery selectors for left/right strafing
+
+$("#leftButton").on("click", function() {
+    navigate("left");
+});
+
+$("#rightButton").on("click", function() {
+    navigate("right");
+});
+
+//For now, strafing is removed, and left and right buttons rotate
+
+// OLD/UNFINISHED CODE
+
+// Keycodes for WASD movement, a feature I hope to add now that button navigation is working:
+
+// w is 87
+// a is 65
+// s is 83
+// d is 68 
+// left arrow is 37 
+// right arrow is 39
+
+// //This function will draw an HTML table of the same dimensions as the array. The CSS file defines the styling of each cell.
+// function mapGenerator() {
+//     $("#mapContainer").append("<table></table>");
+//     $("#mapContainer table").addClass("map");
+    
+//     for (i = 0; i <= museumArray.length - 1; i++) {
+//         $(".map").append("<tr></tr>");
+//         $(".map tr").addClass("row");
+        
+//     }
+
+//     for (j = 0; j <= museumArray[0].length - 1; j++) {
+//         $(".row").append("<td></td>");
+//     }
+
+// };
+
+// mapGenerator();
+
+// //This function can be the starting point for a way to go through the drawn array, and apply it to the HTML table
+// //The objective is to only draw the cells that the user can navigate, leaving all the undefined arrays blank
+// function checkArray() {
+//     for (i = 0; i <= museumArray.length - 1; i++) {
+//         for (j = 0; j <= museumArray[i].length - 1; j++) {
+//             if (museumArray[i][j][0] === undefined) {
+//                 console.log("undefined");
+//             } else {
+//                 console.log("defined");
+//             }
+//         }
+//     }
+// };
