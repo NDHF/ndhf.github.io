3efffa15cd0f5b062f4a3377b5386c9360007047
Nicholas Bernhard <nicholas@NDHFilms.com>
2018-08-28 01:36:01 -0600

    Greatly simplified the get-location function, and changed how arrays look

--- navigatecells/logic.js
+++ navigatecells/logic.js
@@ -1,15 +1,15 @@
 // A simple program for navigating a multidimensional array
 
 //Even though I am navigating in two-dimensional space, my array has three dimensions:
-    //One for going up and down
-    //One for going side to side
-    //One for rotating in place
+//One for going up and down
+//One for going side to side
+//One for rotating in place
 
 
 // Within the sub-sub-arrays:
-    //           0 = north
-    // west = 3     1 = east
-    //           2 = south
+//           0 = north
+// west = 3     1 = east
+//           2 = south
 
 // For example, each index in the array can be an HTML img tag of a perspective in a room, 
 // allowing the user to click through an area, like in the game Myst
@@ -48,7 +48,7 @@ function movementConditionals(isThereAWall, movementForward, movementBackward) {
     if ((movementBackward === undefined) || (isThereAWall === "wall")) {
         canMoveBackward = false;
     } else {
-        canMoveBackward = true; 
+        canMoveBackward = true;
     }
 };
 
@@ -56,16 +56,16 @@ function movementConditionals(isThereAWall, movementForward, movementBackward) {
 
 function checkMovements(array) {
 
-    var arrayForwardAtZ0 = array[(x - 1)][y][z];
-    var arrayBackwardAtZ0 = array[(x + 1)][y][z];
-    var arrayForwardAtZ1 = array[x][(y + 1)][z];
-    var arrayBackwardAtZ1 = array[x][(y - 1)][z];
+    var arrayForwardAtZ0 = array[(x - 1)][y].zArray;
+    var arrayBackwardAtZ0 = array[(x + 1)][y].zArray;
+    var arrayForwardAtZ1 = array[x][(y + 1)].zArray;
+    var arrayBackwardAtZ1 = array[x][(y - 1)].zArray;
     var arrayForwardAtZ2 = arrayBackwardAtZ0;
     var arrayBackwardAtZ2 = arrayForwardAtZ0;
     var arrayForwardAtZ3 = arrayBackwardAtZ1;
     var arrayBackwardAtZ3 = arrayForwardAtZ1;
 
-    var isThereAWall = array[x][y][z];
+    var isThereAWall = array[x][y].zArray[z];
 
     var forwardBackwardArray = [
         [arrayForwardAtZ0, arrayBackwardAtZ0],
@@ -102,9 +102,9 @@ function changeButtons() {
 function buttonStatus(button, onOrOff) {
 
     if (onOrOff === "off") {
-    $(button).css("background-color", "gray");
+        $(button).css("background-color", "gray");
     } else if (onOrOff === "on") {
-    $(button).css("background-color", "blue");
+        $(button).css("background-color", "blue");
     }
 };
 
@@ -115,33 +115,33 @@ function infoPackage() {
     navigateAudio();
     showCoordinates();
     checkMovements(museumArray);
-    getLocation();
+    getLocation(museumArray);
 };
 
 // jQuery selectors for navigation buttons
 
-$("#forwardButton").on("click", function() {
+$("#forwardButton").on("click", function () {
     direction = "forward";
     navigate(direction);
 });
 
-$("#backwardButton").on("click", function() {
+$("#backwardButton").on("click", function () {
     direction = "backward";
     navigate(direction);
 });
 
-$("#leftButton").on("click", function() {
+$("#leftButton").on("click", function () {
     direction = "ccw";
     navigate(direction);
 });
 
-$("#rightButton").on("click", function() {
+$("#rightButton").on("click", function () {
     direction = "cw";
     navigate(direction);
 });
 
-$(document).on("keydown", function(event) {
-    
+$(document).on("keydown", function (event) {
+
     console.log("You pressed the " + event.originalEvent.key + " key!");
 
     if ((event.originalEvent.key === "w") || (event.originalEvent.key === "ArrowUp")) {
@@ -156,7 +156,7 @@ $(document).on("keydown", function(event) {
     } else if ((event.originalEvent.key === "d") || (event.originalEvent.key === "ArrowRight")) {
         direction = "cw";
         navigate(direction);
-    } 
+    }
 });
 
 function navigateAudio() {
@@ -183,30 +183,30 @@ function navigate(direction) {
         console.log("Not permitted");
     } else if (z === 0) {
         if ((direction === "forward") && (canMoveForward === true)) {
-                x--;
-            } else if ((direction === "backward") && (canMoveBackward === true)) {
-                x++;
-            }
+            x--;
+        } else if ((direction === "backward") && (canMoveBackward === true)) {
+            x++;
+        }
     } else if (z === 1) {
         if ((direction === "forward") && (canMoveForward === true)) {
-                y++;
-            } else if ((direction === "backward") && (canMoveBackward === true)) {
-                y--;
-            }
+            y++;
+        } else if ((direction === "backward") && (canMoveBackward === true)) {
+            y--;
+        }
     } else if (z === 2) {
         if ((direction === "forward") && (canMoveForward === true)) {
-                x++;
-            } else if ((direction === "backward") && (canMoveBackward === true)) {
-                x--;
-            }
-        } else if (z === 3) {
+            x++;
+        } else if ((direction === "backward") && (canMoveBackward === true)) {
+            x--;
+        }
+    } else if (z === 3) {
         if ((direction === "forward") && (canMoveForward === true)) {
-                y--;
-            } else if ((direction === "backward") && (canMoveBackward === true)) {
-                y++;
-            }
-        } 
-     
+            y--;
+        } else if ((direction === "backward") && (canMoveBackward === true)) {
+            y++;
+        }
+    }
+
     if (direction === "cw") {
         if (z === 3) {
             z = 0;
@@ -214,17 +214,17 @@ function navigate(direction) {
             z++;
         }
     }
-    
+
     if (direction === "ccw") {
         if (z === 0) {
             z = 3;
         } else {
             z--;
-        }  
-    }   
+        }
+    }
 
     infoPackage();
-}; 
+};
 
 // This function will give the user a message based on their position.
 // The general if-statement takes two coordinates to show which cell the user is in.
@@ -232,128 +232,23 @@ function navigate(direction) {
 
 // This function can also be used to add ambient audio based on room, and load photos/video
 
-function getLocation() {
-    if ((x === 5) && (y === 2)) {
-        console.log("You are in the front yard.");
-        $("#xyCoordinateText").html("You are in the front yard.");
-        if (z === 0) {
-            console.log("You are facing towards the front door.");
-            $("#picture").attr("src", "images/x5y2z0.JPG");
-        } else if (z === 1) {
-            console.log("You are looking west on Simpson Street toward Public Road.");
-        } else if (z === 2) {
-            console.log("You are facing away from the front door.");
-        } else if (z === 3) {
-            console.log("You are looking east on Simpson Street, toward the historic business district.");
-        } 
-    }
-
-    if ((x === 4) && (y === 1)) {
-        console.log("You are in the business exhibit.");
-        $("#xyCoordinateText").html("You are in the business exhibit.");
-        if (z === 0) {
-            console.log("You are facing towards our upcoming Notable Persons exhibit.");
-            $("#picture").attr("src", "images/x4y1z0.JPG");
-        } else if (z === 1) {
-            console.log("You are facing towards the foyer.");
-            $("#picture").attr("src", "images/x4y1z1.JPG");
-        } else if (z === 2) {
-            console.log("You are facing our historic businesses exhbit, and the harmonium.");
-            $("#picture").attr("src", "images/x4y1z2.JPG");
-        } else if (z === 3) {
-            console.log("You are looking towards the window.");
-            $("#picture").attr("src", "images/x4y1z3.JPG");
-        } 
-
-    }
-
-    if ((x === 4) && (y === 2)) {
-        console.log("You are in the foyer.");
-        $("#xyCoordinateText").html("You are in the foyer.");
-        if (z === 0) {
-            console.log("You are facing toward the mining equipment exhibit. You can see the house's original colonade feature, and above it, the check tag board from the Black Diamond Mine.");
-            $("#picture").attr("src", "images/x4y2z0.JPG");
-        } else if (z === 1) {
-            console.log("You are facing towards our displays for Lafayette High School, the Great Snowstorm of 1913, and the Lafayette Elementary fire of 1964");
-            $("#picture").attr("src", "images/x4y2z1.JPG");
-        } else if (z === 2) {
-            console.log("You are facing toward Simpson Street. You can see a display on the Lewis House, and the Black Diamond Mine");
-            $("#picture").attr("src", "images/x4y2z2.JPG");
-        } else if (z === 3) {
-            console.log("You are looking toward our Dr. Wolf bookcase, and the business exhibit.");
-            $("#picture").attr("src", "images/x4y2z3.JPG");
-        } 
-    }
-
-    if ((x === 3) && (y === 1)) {
-        console.log("You are in the high school exhibit.");
-        $("#xyCoordinateText").html("You are in the high school exhibit.");
-        if (z === 0) {
-            console.log("You are facing towards our Johnny Manazanares Trophy case. These trophies were won by Lafayette High Students.");
-            $("#picture").attr("src", "images/x3y1z0.JPG");
-        } else if (z === 1) {
-            console.log("You are looking back toward the mining equipment room. On the right are two baseball jerseys from Lafayette's past.");
-            $("#picture").attr("src", "images/x3y1z1.JPG");
-        } else if (z === 2) {
-            console.log("You are looking towards our TV, where we show the short, two-part documentary Stories of Lafayette.");
-            $("#picture").attr("src", "images/x3y1z2.JPG");
-        } else if (z === 3) {
-            console.log("You are looking at our sliding display, with a collection of facts and photos from city history.");
-            $("#picture").attr("src", "images/x3y1z3.JPG");
-        }    
-    }
-
-    if ((x === 3) && (y === 2)) {
-        console.log("You are in the mining equipment exhibit.");
-        $("#xyCoordinateText").html("You are in the Mining Equipment Exhibit.");
-        if (z === 0) {
-            console.log("You are facing towards our scale model of the Simpson Mine, and displays about historic Lafayette mines.");
-            $("#picture").attr("src", "images/x3y2z0.JPG");
-        } else if (z === 1) {
-            console.log("You are facing towards our maps of Lafayette coal mines, and a mining tool display.");
-            $("#picture").attr("src", "images/x3y2z1.JPG");
-        } else if (z === 2) {
-            console.log("You are looking back towards the foyer.");
-            $("#picture").attr("src", "images/x3y2z2.JPG");
-        } else if (z === 3) {
-            console.log("You are looking at our upright mine tool display, featuring hard hats, lanterns, and samples of coal. To the right of the case, you may enter the high school exhibit.");
-            $("#picture").attr("src", "images/x3y2z3.JPG");
-        }    
-    }
-
-    if ((x === 2) && (y === 2)) {
-        console.log("You are in the kitchen exhibit.");
-        $("#xyCoordinateText").html("You are in the kitchen exhibit.");
-        if (z === 0) {
-            console.log("You are looking towards our back porch, containing our farming exhibit.");
-            $("#picture").attr("src", "images/x2y2z0.JPG");
-        } else if (z === 1) {
-            console.log("You are looking at our coffee machine, and a map of the Northern Coal Field from the late 1970s.");
-            $("#picture").attr("src", "images/x2y2z1.JPG");
-        } else if (z === 2) {
-            console.log("You are looking at our glassware display, featuring a china set owned by town founder Mary Miller, sets of depression and carnival glass, and glass containing uranium. To the left is a doorway back to the mining equipment exhibit");
-            $("#picture").attr("src", "images/x2y2z2.JPG");
-        } else if (z === 3) {
-            console.log("You are facing towards our restroom. The pantry is on the right.");
-            $("#picture").attr("src", "images/x2y2z3.JPG");
-        } 
-    }
-
-    if ((x === 1) && (y === 2)) {
-        console.log("You are in the farming exhibit.");
-        $("#xyCoordinateText").html("You are in the farming exhibit.");
-        if (z === 0) {
-            console.log("You are facing towards our museum back door. On the left is a two-man timber saw, about six feet long");
-            $("#picture").attr("src", "images/x1y2z0.JPG");
-        } else if (z === 1) {
-            console.log("You are facing towards our farming tool display.");
-            $("#picture").attr("src", "images/x1y2z1.JPG");
-        } else if (z === 2) {
-            console.log("On the left, a doorway leads back to the kitchen exhibit. In front of you is a wooden table, brought here by covered wagon in the 1860s.");
-            $("#picture").attr("src", "images/x1y2z2.JPG");
-        } else if (z === 3) {
-            console.log("You are looking towards Harrison Avenue. In front of the window is a hand-powered washing machine, and to the left, along the wall, is an old wine press.");
-            $("#picture").attr("src", "images/x1y2z3.JPG");
-        } 
+function getLocation(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (x === i) {
+            console.log(array[x]);
+            for (let j = 0; j < array[i].length; j++) {
+                if (y === j) {
+                    console.log(array[x][y]);
+                    console.log(array[x][y].zArray);
+                    console.log(array[x][y].zArray.length);
+                    $("p#xyCoordinateText").html(array[x][y].RoomText);
+                    for (let k = 0; k < array[i][j].zArray.length; k++) {
+                        if (z === k) {
+                            $("#picture").attr("src", "images/x" + x + "y" + y + "z" + z + ".JPG");
+                        }
+                    }
+                }
+            }
+        }
     }
 };
--- navigatecells/museumArray.js
+++ navigatecells/museumArray.js
@@ -4,16 +4,18 @@ var northWall = ["wall", 1, 2, 3];
 
 var southWall = [0, 1, "wall", 3];
 
+var undefinedObject = {zArray: undefined};
+
 // var standardRoom = [1, 2, 3, 4];
 
 var museumArray = [
 
-    [ [], [          ], [          ], [] ],
-    [ [], [          ], standardRoom, [] ],
-    [ [], [          ], standardRoom, [] ],
-    [ [], southWall, standardRoom, [] ],
-    [ [], northWall, standardRoom, [] ],
-    [ [], [          ], standardRoom, [] ],
-    [ [], [          ], [          ], [] ]
+    [ undefinedObject, undefinedObject, undefinedObject, undefinedObject ],
+    [ undefinedObject, undefinedObject, {RoomText: "You are in the farming exhibit", zArray: standardRoom}, undefinedObject],
+    [ undefinedObject, undefinedObject, {RoomText: "You are in the kitchen", zArray: standardRoom}, undefinedObject ],
+    [ undefinedObject, {RoomText: "You are in the high school room", zArray: southWall}, {RoomText: "You are in the mining equipment Room", zArray: standardRoom}, undefinedObject ],
+    [ undefinedObject, {RoomText: "You are in the business exhibit", zArray: northWall}, {RoomText: "You are in the foyer.", zArray: standardRoom}, undefinedObject ],
+    [ undefinedObject, undefinedObject, {RoomText: "You are in the front yard.", zArray: standardRoom}, undefinedObject ],
+    [ undefinedObject, undefinedObject, undefinedObject, undefinedObject ]
 
     ];
